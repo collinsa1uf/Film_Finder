ff5dab2f69c603eb4cd26290b56bb45dc9c394de
collinsa1uf <collinsa1@ufl.edu>
2024-04-18 17:46:02 -0400

    - fixed dropdown menus - added movie class

--- film_finder.py
+++ film_finder.py
@@ -1,13 +1,14 @@
 from matplotlib.pyplot import *
 from networkx import *
 from tkinter import *
+from movie import *
 from algorithms import *
 from data_structures import *
 matplotlib.use('TkAgg')
 
 
 # checks if the movie title inputted by the user is in the list of movies in "movies.txt"
-def check_movie(user_input):
+def validate_movie(user_input):
     file = open('movies.txt', 'r', errors='ignore')
 
     while True:
@@ -17,9 +18,9 @@ def check_movie(user_input):
             break
         else:
             movie_info = movie_info.split('\t')
-            movie = movie_info[0]
+            movie_title = movie_info[0]
 
-            if movie == user_input:
+            if movie_title == user_input:
                 return True
 
     file.close()
@@ -59,19 +60,23 @@ def main():
     outcome = Label(window, text='', font=('Times New Roman', 10), bg='#40a6c2')
     outcome.place(x=713, y=155)
 
-    # gets input from textbox
+    # creates movie object
+    movie = Movie()
+
+    # gets input from textbox and validates it
     def check_input(event=None):
         user_input = textbox.get()
         textbox.delete(0, 'end')
 
-        movie_found = check_movie(user_input)
+        movie_found = validate_movie(user_input)
 
         if movie_found:
-            text = 'You entered \"' + user_input + '\"'
-            outcome.config(text=text, fg='black')
+            successful_text = 'You entered \"' + user_input + '\"'
+            outcome.config(text=successful_text, fg='black')
+            movie.set_title(user_input)
         else:
-            text = 'Movie title not found. Please enter another movie title.'
-            outcome.config(text=text, fg='#cc3f14')
+            unsuccessful_text = 'Movie title not found. Please enter another movie title.'
+            outcome.config(text=unsuccessful_text, fg='#cc3f14')
 
     textbox.bind('<Return>', check_input)
 
@@ -80,43 +85,60 @@ def main():
     prompt2.place(x=760, y=220)
 
     # creates dropdown menus
-    priorities = ['None', '1', '2', '3', '4', '5', '6', '7']
-    clicked_option = StringVar()
-    clicked_option.set(priorities[0])
-
+    priorities1 = ['None', '1', '2', '3', '4', '5', '6', '7']
+    clicked_option1 = StringVar()
+    clicked_option1.set(priorities1[0])
     text1 = Label(window, text='Genre   -', font=('Times New Roman', 16), bg='#40a6c2', fg='black')
     text1.place(x=800, y=270)
-    dropdown1 = OptionMenu(window, clicked_option, *priorities)
+    dropdown1 = OptionMenu(window, clicked_option1, *priorities1)
     dropdown1.place(x=890, y=270)
 
+    priorities2 = ['None', '1', '2', '3', '4', '5', '6', '7']
+    clicked_option2 = StringVar()
+    clicked_option2.set(priorities2[0])
     text2 = Label(window, text='Year   -', font=('Times New Roman', 16), bg='#40a6c2', fg='black')
     text2.place(x=810, y=310)
-    dropdown2 = OptionMenu(window, clicked_option, *priorities)
+    dropdown2 = OptionMenu(window, clicked_option2, *priorities2)
     dropdown2.place(x=890, y=310)
 
+    priorities3 = ['None', '1', '2', '3', '4', '5', '6', '7']
+    clicked_option3 = StringVar()
+    clicked_option3.set(priorities3[0])
     text3 = Label(window, text='Runtime   -', font=('Times New Roman', 16), bg='#40a6c2', fg='black')
     text3.place(x=778, y=350)
-    dropdown3 = OptionMenu(window, clicked_option, *priorities)
+    dropdown3 = OptionMenu(window, clicked_option3, *priorities3)
     dropdown3.place(x=890, y=350)
 
+    priorities4 = ['None', '1', '2', '3', '4', '5', '6', '7']
+    clicked_option4 = StringVar()
+    clicked_option4.set(priorities4[0])
     text4 = Label(window, text='Main Stars   -', font=('Times New Roman', 16), bg='#40a6c2', fg='black')
     text4.place(x=761, y=390)
-    dropdown4 = OptionMenu(window, clicked_option, *priorities)
+    dropdown4 = OptionMenu(window, clicked_option4, *priorities4)
     dropdown4.place(x=890, y=390)
 
+    priorities5 = ['None', '1', '2', '3', '4', '5', '6', '7']
+    clicked_option5 = StringVar()
+    clicked_option5.set(priorities5[0])
     text5 = Label(window, text='Director   -', font=('Times New Roman', 16), bg='#40a6c2', fg='black')
     text5.place(x=780, y=430)
-    dropdown5 = OptionMenu(window, clicked_option, *priorities)
+    dropdown5 = OptionMenu(window, clicked_option5, *priorities5)
     dropdown5.place(x=890, y=430)
 
+    priorities6 = ['None', '1', '2', '3', '4', '5', '6', '7']
+    clicked_option6 = StringVar()
+    clicked_option6.set(priorities6[0])
     text6 = Label(window, text='Reviews (IMDb)   -', font=('Times New Roman', 16), bg='#40a6c2', fg='black')
     text6.place(x=711, y=470)
-    dropdown6 = OptionMenu(window, clicked_option, *priorities)
+    dropdown6 = OptionMenu(window, clicked_option6, *priorities6)
     dropdown6.place(x=890, y=470)
 
+    priorities7 = ['None', '1', '2', '3', '4', '5', '6', '7']
+    clicked_option7 = StringVar()
+    clicked_option7.set(priorities7[0])
     text7 = Label(window, text='Rating   -', font=('Times New Roman', 16), bg='#40a6c2', fg='black')
     text7.place(x=796, y=510)
-    dropdown7 = OptionMenu(window, clicked_option, *priorities)
+    dropdown7 = OptionMenu(window, clicked_option7, *priorities7)
     dropdown7.place(x=890, y=510)
 
     submit_button = Button(window, text='Submit', font=('Times New Roman', 12))
---
+++ movie.py
@@ -0,0 +1,8 @@
+class Movie:
+    title = ''
+
+    def set_title(self, title):
+        self.title = title
+
+    def get_title(self):
+        return self.title
